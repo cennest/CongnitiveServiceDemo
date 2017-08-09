9aba6d682b0e7c6e651413861b573e05bd14b58d
Moorthy <moorthy.yadav@cennest.com>
2017-08-09 19:24:07 +0530

    TTS changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpModule, Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 import 'rxjs/Rx';
 
-
 @Injectable()
 export class ApiService {
-  results:string;
-  constructor(private http:Http) { }
 
-  convertTextToSpeech(textToConvert : string) {
+  constructor(private http: Http) { }
+
+  getToken() {
+    const url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken';
+
+    let headers = new Headers({ "Content-Type": "application/json" });
+    headers.append("Accept", "application/json");
+    headers.append("Ocp-Apim-Subscription-Key", 'decd0a3d9a5742c9aca5a18066ea65b8');
+
+    let options = new RequestOptions({ headers: headers });
+
     // alert(textToConvert);
-    var headers = new Headers({'Ocp-Apim-Subscription-Key': 'decd0a3d9a5742c9aca5a18066ea65b8'});
+    return this.http.post(url, {}, options).map(data => data.text());
+  }
 
-    this.http.post("https://api.cognitive.microsoft.com/sts/v1.0/issueToken",
-    "",headers).subscribe(data => {
-      // Read the result field from the JSON response.
-     this.results=data.text.toString();
-     console.log(this.results);
-    });
+  convertTextToSpeech(token: string, textToConvert: string) {
+    const url = 'https://speech.platform.bing.com/synthesize';
+
+    let headers = new Headers({ 'Content-Type': 'application/ssml+xml' });
+    headers.append('X-Microsoft-OutputFormat', 'riff-16khz-16bit-mono-pcm');
+    headers.append('Authorization', `Bearer ${token}`);
+    headers.append('X-Search-AppId', '07D3234E49CE426DAA29772419F436CA');
+    headers.append('X-Search-ClientID', '1ECFAE91408841A480F00935DC390960')
+
+    let options = new RequestOptions({ headers: headers });
+
+    // alert(textToConvert);
+    return this.http.post(url, `<speak version="1.0" xml:lang="en-US">
+    <voice xml:lang="en-US" xml:gender="Female" name="Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)">${textToConvert}</voice>
+</speak>`
+      , options).map(data => data.arrayBuffer());
   }
 }
--- src/app/text-to-speech/text-to-speech.component.ts
+++ src/app/text-to-speech/text-to-speech.component.ts
@@ -1,25 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../services/api.service";
-
+import { wav } from 'wav';
+import { Speaker } from 'speaker';
 
 @Component({
   selector: 'text-to-speech',
   templateUrl: './text-to-speech.component.html',
   styleUrls: ['./text-to-speech.component.css']
 })
-export class TextToSpeechComponent implements OnInit {
+export class TextToSpeechComponent {
 
-textToConvert:string;
-  constructor(private apiService: ApiService) { 
+  textToConvert: string;
+  audioContext: AudioContext;
+  curBuffer;
+
+  constructor(private apiService: ApiService) {
+    this.audioContext = new AudioContext();
   }
+
   convertToAudioBtnClicked() {
-      this.textToConvert = (<HTMLTextAreaElement>document.getElementById("txtArea")).value;
+    this.textToConvert = (<HTMLTextAreaElement>document.getElementById("txtArea")).value;
 
-    this.apiService.convertTextToSpeech(this.textToConvert);
-}
-  ngOnInit() {
-    
+    this.apiService.getToken().subscribe(data => {
+      this.onTokenReceived(data, this.textToConvert);
+    });
   }
-  
 
+  onTokenReceived(token, text) {
+    this.apiService.convertTextToSpeech(token, text).subscribe(data => {
+      console.log(data);
+      this.audioContext.decodeAudioData(data, (buf) => {
+        let bufferSource = this.audioContext.createBufferSource();
+        bufferSource.buffer = buf;
+        bufferSource.connect(this.audioContext.destination);
+        bufferSource.start(0);
+      });
+    });
+  }
 }
